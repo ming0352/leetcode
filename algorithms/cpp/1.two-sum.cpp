8c5db053ea592a77243b7ae62ddb3eb141224992
ming0352 <ming.z820620@gmail.com>
2022-07-15 23:32:02 +0800

    Changes to be committed: 	modified:   algorithms/cpp/1.two-sum.cpp

--- algorithms/cpp/1.two-sum.cpp
+++ algorithms/cpp/1.two-sum.cpp
@@ -5,23 +5,41 @@
  */
 
 // @lc code=start
+//  O(n^2)
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         vector<int> answer;
+//         for(int i=0 ; i<nums.size() ;i++)
+//         {
+//             for(int j=i+1;j<nums.size();j++){
+//                if(nums[i]+nums[j] == target)
+//                {
+//                    answer.push_back(i);
+//                    answer.push_back(j);
+//                }
+//             }
+//         }
+//         return answer;
+//     }
+
+// };
+//O(N)
 class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> answer;
-        for(int i=0 ; i<nums.size() ;i++)
-        {
-            for(int j=i+1;j<nums.size();j++){
-               if(nums[i]+nums[j] == target)
-               {
-                   answer.push_back(i);
-                   answer.push_back(j);
-               }
-            }        
-        }
-        return answer;
+ public:
+  vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> answer;
+    unordered_map<int, int> hash;
+    for (int i = 0; i < nums.size(); i++) {
+      if (hash.find(target - nums[i]) != hash.end() &&
+          i != hash[target - nums[i]]) {
+        answer.emplace_back(i);
+        answer.emplace_back(hash[target - nums[i]]);
+        break;
+      }
+      hash.insert(make_pair(nums[i], i));
     }
-    
+    return answer;
+  }
 };
 // @lc code=end
-
