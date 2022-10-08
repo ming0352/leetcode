2e39a82abde6c08ce4c1de342c83bae3c455c097
ming0352 <ming.z820620@gmail.com>
2022-10-08 10:18:23 +0800

    Changes to be committed: 	modified:   algorithms/cpp/459.repeated-substring-pattern.cpp

--- algorithms/cpp/459.repeated-substring-pattern.cpp
+++ algorithms/cpp/459.repeated-substring-pattern.cpp
@@ -7,6 +7,35 @@
 // @lc code=start
 class Solution {
   public:
+    int *next = new int[10000];
+    void build_next(string s) {
+        int j = 0;
+        next[0] = 0;
+        for (int i = 1; i < s.size(); i++) {
+            while (j > 0 && s[i] != s[j]) {
+                j = next[j - 1];
+            }
+            if (s[i] == s[j]) {
+                j++;
+            }
+            next[i] = j;
+        }
+    }
+    bool repeatedSubstringPattern(string s) {
+
+        build_next(s);
+        int len = s.size();
+        if (next[len - 1] != 0 && (len % (len - next[len - 1])) == 0)
+            return true;
+
+        return false;
+    }
+};
+
+// method 2
+/*
+class Solution {
+  public:
     bool repeatedSubstringPattern(string s) {
         string tmp = s + s;
         tmp.erase(tmp.begin());
@@ -17,4 +46,5 @@ class Solution {
             return false;
     }
 };
+*/
 // @lc code=end
