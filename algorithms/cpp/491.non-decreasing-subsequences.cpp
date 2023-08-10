a726c6727cf15b1bd294e60e89a914752a419cf8
ming0352 <ming.z820620@gmail.com>
2023-08-10 08:57:02 +0800

    modified no.491

--- algorithms/cpp/491.non-decreasing-subsequences.cpp
+++ algorithms/cpp/491.non-decreasing-subsequences.cpp
@@ -4,59 +4,27 @@
  * [491] Non-decreasing Subsequences
  */
 
-// @lc code=start
-// class Solution {
-//   public:
-//     vector<vector<int>> result;
-//     vector<int> path;
-//     void backtracking(vector<int> &nums, int idx, vector<bool> &used) {
-//         if (path.size() >= 2) {
-//             result.push_back(path);
-//         }
-//         for (int i = idx; i < nums.size(); i++) {
-//             if(i>0 &&nums[i] < nums[i - 1]) continue;
-//             if (i > 0 && nums[i] == nums[i - 1] && used[i - 1] == false) {
-//                 continue;
-//             }
-            
-//             path.push_back(nums[i]);
-//             used[i] = true;
-//             backtracking(nums, i + 1, used);
-//             path.pop_back();
-//             used[i] = false;
-//         }
-//     }
-//     vector<vector<int>> findSubsequences(vector<int> &nums) {
-//         vector<bool> used = {false};
-//         used.resize(nums.size());
-//         backtracking(nums, 0, used);
-//         return result;
-//     }
-// };
-class Solution {
-private:
+@lc code = start class Solution {
+  public:
     vector<vector<int>> result;
     vector<int> path;
-    void backtracking(vector<int>& nums, int startIndex) {
-        if (path.size() > 1) {
+    void backtracking(vector<int> &nums, int idx) {
+        if (path.size() >= 2) {
             result.push_back(path);
         }
-        unordered_set<int> uset; 
-        for (int i = startIndex; i < nums.size(); i++) {
-            if ((!path.empty() && nums[i] < path.back())
-                    || uset.find(nums[i]) != uset.end()) {
-                    continue;
+        unordered_set<int> uset;
+        for (int i = idx; i < nums.size(); i++) {
+            if ((!path.empty() && nums[i] < path.back()) ||
+                (uset.find(nums[i]) != uset.end())) {
+                continue;
             }
-            uset.insert(nums[i]); 
+            uset.insert(nums[i]);
             path.push_back(nums[i]);
             backtracking(nums, i + 1);
             path.pop_back();
         }
     }
-public:
-    vector<vector<int>> findSubsequences(vector<int>& nums) {
-        result.clear();
-        path.clear();
+    vector<vector<int>> findSubsequences(vector<int> &nums) {
         backtracking(nums, 0);
         return result;
     }
