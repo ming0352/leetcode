eb66f5cdd76c1b2fd329ed8aac662d5d27d89c12
ming0352 <ming.z820620@gmail.com>
2023-08-12 09:34:43 +0800

    update no.47

--- algorithms/cpp/47.permutations-ii.cpp
+++ algorithms/cpp/47.permutations-ii.cpp
@@ -6,31 +6,35 @@
 
 // @lc code=start
 class Solution {
-public:
+  public:
     vector<vector<int>> result;
     vector<int> path;
-    
-    void backtracking(vector<int>& nums,vector<bool> used){
-        if(path.size()==nums.size()){
+
+    void backtracking(vector<int> &nums, vector<bool> used) {
+        if (path.size() == nums.size()) {
             result.push_back(path);
-            return; 
+            return;
         }
-        unordered_set<int> uset;
-        for(int i=0;i<nums.size();i++){
-            if((!path.empty()&&used[i])|| uset.find(nums[i])!=uset.end()) continue;
-            used[i]=true;
+        // unordered_set<int> uset;
+        for (int i = 0; i < nums.size(); i++) {
+            // if((!path.empty()&&used[i])|| uset.find(nums[i])!=uset.end())
+            // continue;
+            if (i>0 && nums[i]==nums[i-1] && used[i-1]==false) continue;
+            if(used[i]==false){
+            used[i] = true;
             path.push_back(nums[i]);
-            backtracking(nums,used);
+            backtracking(nums, used);
             path.pop_back();
-            used[i]=false;
-            uset.insert(nums[i]);
+            used[i] = false;
+            //uset.insert(nums[i]);
+            }
         }
     }
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<bool> used(nums.size(),false);
-        backtracking(nums,used);
+    vector<vector<int>> permuteUnique(vector<int> &nums) {
+        sort(nums.begin(), nums.end());
+        vector<bool> used(nums.size(), false);
+        backtracking(nums, used);
         return result;
     }
 };
 // @lc code=end
-
