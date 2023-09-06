a3619628f15471c00a4c7510c2d8047176e3d45d
ming0352 <ming.z820620@gmail.com>
2023-09-06 22:13:50 +0800

    update no.416

--- algorithms/cpp/416.partition-equal-subset-sum.cpp
+++ algorithms/cpp/416.partition-equal-subset-sum.cpp
@@ -6,24 +6,24 @@
 
 // @lc code=start
 class Solution {
-public:
-    bool canPartition(vector<int>& nums) {
+  public:
+    bool canPartition(vector<int> &nums) {
         int sum = 0;
-        vector<int> dp(10001, 0);
         for (int i = 0; i < nums.size(); i++) {
             sum += nums[i];
         }
-        if (sum % 2 == 1) return false;
+        if (sum % 2 == 1)
+            return false;
         int target = sum / 2;
-
-        for(int i = 0; i < nums.size(); i++) {
-            for(int j = target; j >= nums[i]; j--) {
+        vector<int> dp(10001, 0);
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = target; j >= nums[i]; j--) {
                 dp[j] = max(dp[j], dp[j - nums[i]] + nums[i]);
             }
         }
-        if (dp[target] == target) return true;
+        if (dp[target] == target)
+            return true;
         return false;
     }
 };
 // @lc code=end
-
