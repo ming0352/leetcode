538e064e016048273a83e792d456101e73870f77
ming0352 <ming.z820620@gmail.com>
2023-09-14 23:16:34 +0800

    update no.70

--- algorithms/cpp/70.climbing-stairs.cpp
+++ algorithms/cpp/70.climbing-stairs.cpp
@@ -4,16 +4,30 @@
  * [70] Climbing Stairs
  */
 
-// @lc code=start
+//@lc code=start
+// class Solution {
+//   public:
+//     int climbStairs(int n) {
+//         if (n<=2) return n;
+//         vector<int> dp(n + 1);
+//         dp[1] = 1;
+//         dp[2] = 2;
+//         for (int i = 3; i <= n; i++) {
+//             dp[i] = dp[i - 1] + dp[i - 2];
+//         }
+//         return dp[n];
+//     }
+// };
+
 class Solution {
   public:
     int climbStairs(int n) {
-        if (n<=2) return n;
-        vector<int> dp(n + 1);
-        dp[1] = 1;
-        dp[2] = 2;
-        for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) { 
+            for (int j = 1; j <= 2; j++) { 
+                if (i - j >= 0) dp[i] += dp[i - j];
+            }
         }
         return dp[n];
     }
