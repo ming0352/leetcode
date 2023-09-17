65fbb17c4e05b8188c591986ccf9497ed6b69b6c
ming0352 <ming.z820620@gmail.com>
2023-09-17 23:20:00 +0800

    update no.279

--- algorithms/cpp/279.perfect-squares.cpp
+++ algorithms/cpp/279.perfect-squares.cpp
@@ -9,10 +9,10 @@ class Solution {
 public:
     int numSquares(int n) {
         vector<int> dp(n + 1, INT_MAX);
-        dp[0] = 0;
-        for (int i = 0; i <= n; i++) { 
-            for (int j = 1; j * j <= i; j++) { 
-                dp[i] = min(dp[i - j * j] + 1, dp[i]);
+        dp[0]=0;
+        for(int i=0;i<=n;i++){
+            for(int j=1;j*j<=i;j++){
+                dp[i]=min(dp[i-j*j]+1,dp[i]);
             }
         }
         return dp[n];
