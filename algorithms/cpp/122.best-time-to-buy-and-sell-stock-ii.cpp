3961d2c883d6b6acd29e17025c81137a8aa39f86
ming0352 <ming.z820620@gmail.com>
2023-09-23 23:25:11 +0800

    update no.122

--- algorithms/cpp/122.best-time-to-buy-and-sell-stock-ii.cpp
+++ algorithms/cpp/122.best-time-to-buy-and-sell-stock-ii.cpp
@@ -5,17 +5,31 @@
  */
 
 // @lc code=start
+// class Solution {
+//   public:
+//     int maxProfit(vector<int> &prices) {
+//         int result = 0;
+//         for (int i = 1; i < prices.size(); i++) {
+//             int diff = prices[i] - prices[i - 1];
+//             if (diff > 0) {
+//                 result += diff;
+//             }
+//         }
+//         return result;
+//     }
+// };
 class Solution {
-  public:
-    int maxProfit(vector<int> &prices) {
-        int result = 0;
-        for (int i = 1; i < prices.size(); i++) {
-            int diff = prices[i] - prices[i - 1];
-            if (diff > 0) {
-                result += diff;
-            }
+public:
+    int maxProfit(vector<int>& prices) {
+        int len = prices.size();
+        vector<vector<int>> dp(len, vector<int>(2, 0));
+        dp[0][0] -= prices[0];
+        dp[0][1] = 0;
+        for (int i = 1; i < len; i++) {
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
         }
-        return result;
+        return dp[len - 1][1];
     }
 };
 // @lc code=end
