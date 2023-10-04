280ec605758b3b80333b1d6cd3a063a1cfbda495
ming0352 <ming.z820620@gmail.com>
2023-10-04 23:14:03 +0800

    update no.309

---
+++ algorithms/cpp/309.best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=309 lang=cpp
+ *
+ * [309] Best Time to Buy and Sell Stock with Cooldown
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if (n == 0) return 0;
+        vector<vector<int>> dp(n, vector<int>(4, 0));
+        dp[0][0] -= prices[0];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = max(dp[i - 1][0], max(dp[i - 1][3] - prices[i], dp[i - 1][1] - prices[i]));
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][3]);
+            dp[i][2] = dp[i - 1][0] + prices[i];
+            dp[i][3] = dp[i - 1][2];
+        }
+        return max(dp[n - 1][3], max(dp[n - 1][1], dp[n - 1][2]));
+    }
+};
+// @lc code=end
+
